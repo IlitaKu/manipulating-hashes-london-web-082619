e4207b00851d055625e8ace30c3663a112429655
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-25 11:03:24 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,9 +17,9 @@ def first_challenge
   #your code here
 contacts['Freddy Mercury'].each do |key, value|
   if key == :favorite_icecream_flavors
-    delete_if value.include?("strawberry")
-      
-  value
+    value.delete_if { |flavour| flavour == "strawberry"}
+    end
+  contacts
 end
 
   #remember to return your newly altered contacts hash!
